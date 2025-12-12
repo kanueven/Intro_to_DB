a4e4db0735d4140225d0fb902a8c10d53704728d
Gathoni <rachaelgathnonin@gmail.com>
2025-12-12 04:32:00 +0300

    added more customers

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,7 @@
 -- Write a script that inserts multiple rows in the table customer (database alx_book_store) in your MySQL server.
-USE alx_book_store;
-INSERT INTO Customers (customer_name, email, address) 
+-- USE alx_book_store;
+INSERT INTO customers (customer_id,customer_name, email, address) 
 VALUES
-(    'Blessing Malik','bmalik@sandtech.com','124 Happiness  Ave'),
-('Obed Ehoneah','eobed@sandtech.com','125 Happiness  Ave'),
-('Nehemial Kamolu','nkamolu@sandtech.com','126 Happiness  Ave');
+('2' ,'Blessing Malik','bmalik@sandtech.com','124 Happiness  Ave'),
+('3', 'Obed Ehoneah','eobed@sandtech.com','125 Happiness  Ave'),
+('4', 'Nehemial Kamolu','nkamolu@sandtech.com','126 Happiness  Ave');
