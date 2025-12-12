4a968e0ce328b02a83adddf4f07b2665324ef795
Gathoni <rachaelgathnonin@gmail.com>
2025-12-12 04:49:04 +0300

    described table book without using describe or explain

--- task_4.sql
+++ task_4.sql
@@ -1,11 +1,13 @@
--- prints the full description of the table books from the database alx_book_store in your MySQL server.
--- You are not allowed to use the DESCRIBE or EXPLAIN statements
+-- task_4.sql
+-- Print the full description of the 'books' table without using DESCRIBE or EXPLAIN
+
 SELECT 
-COLUMN_NAME AS 'COLUMN',
-DATA_TYPE AS 'DATA TYPE',
-IS_NULLABLE AS 'IS NULLABLE',
-COLUMN_KEY AS 'KEY',
-COLUMN_DEFAULT AS 'DEFAULT',
-EXTRA AS 'EXTRA'
+    COLUMN_NAME AS 'COLUMN',
+    DATA_TYPE AS 'DATA_TYPE',
+    IS_NULLABLE AS 'IS_NULLABLE',
+    COLUMN_KEY AS 'KEY',
+    COLUMN_DEFAULT AS 'DEFAULT',
+    EXTRA AS 'EXTRA'
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'books';
