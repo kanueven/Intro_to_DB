caacb66f79e11608522da9d4c8f2b20e792563e3
Gathoni <rachaelgathnonin@gmail.com>
2025-12-12 04:00:41 +0300

    passed the database as an argument to mysql

--- task_5.sql
+++ task_5.sql
@@ -4,5 +4,5 @@
 -- customer_name = Cole Baidoo
 -- email = cbaidoo@sandtech.com
 -- address = 123 Happiness Ave.
-USE alx_book_store;
-INSERT INTO Customers(customer_id, customer_name, email, address) VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+-- USE alx_book_store;
+INSERT INTO Customers( customer_name, email, address) VALUES ( 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
