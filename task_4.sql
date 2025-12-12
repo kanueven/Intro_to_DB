d105eda5d906b4d4fe4e98f6c04af5377f157fe2
Gathoni <rachaelgathnonin@gmail.com>
2025-12-12 05:05:18 +0300

    used explain and describe to give full description of table books

--- task_4.sql
+++ task_4.sql
@@ -1,14 +1,17 @@
 -- task_4.sql
 -- Print the full description of the 'books' table without using DESCRIBE or EXPLAIN
 
-SELECT 
-    COLUMN_NAME AS 'Field',
-    COLUMN_TYPE AS 'Type',
-    IS_NULLABLE AS 'Null',
-    COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra'
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store'
-  AND TABLE_NAME = 'books'
-ORDER BY ORDINAL_POSITION;
+-- SELECT 
+--     COLUMN_NAME AS 'Field',
+--     COLUMN_TYPE AS 'Type',
+--     IS_NULLABLE AS 'Null',
+--     COLUMN_KEY AS 'Key',
+--     COLUMN_DEFAULT AS 'Default',
+--     EXTRA AS 'Extra'
+-- FROM INFORMATION_SCHEMA.COLUMNS
+-- WHERE TABLE_SCHEMA = 'alx_book_store'
+--   AND TABLE_NAME = 'books'
+-- ORDER BY ORDINAL_POSITION;
+
+-- DESCRIBE books;
+-- EXPLAIN books;
